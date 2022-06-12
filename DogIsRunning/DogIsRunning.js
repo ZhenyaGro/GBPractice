82d227db90e727452d004427d611c06f66d2d93b
ZhenyaGro <bon4ios@gmail.com>
2022-06-12 16:26:06 +0300

    Initial commit

---
+++ DogIsRunning/DogIsRunning.js
@@ -0,0 +1,25 @@
+/*
+To do:
+1. Complete the task.
+2. Add a description.
+3. Create animation.
+
+Description:
+We have 2 humans. They are going toward each other with different speed.
+There is a dog it is running 
+*/
+
+'use strict';
+
+class Animal {
+  constructor(speed) {
+    this.speed = speed;
+  }
+}
+
+class Human extends Animal {
+}
+
+class Dog extends Animal {
+
+}
